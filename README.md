484a15b96d6bdbe2c997d48ad55c9e0a79e9b858
LoreVazquez <lorena_vor@hotmail.com>
2018-05-15 20:56:18 -0500

    Se agrega el README del proyecto, se actualiza la estructura, los estilos y funcionalidad para completar la version 0.0.2

---
+++ README.md
@@ -0,0 +1,30 @@
+
+# Reto replicar página Trello
+
+En este reto, se replica el tablero de Trello, para ello se siguen un flujo de versiones que representan los avances del reto.
+
+Para este reto se utilizará de guía el estilo del sitio original.
+
+A continuación se enlistan los versiones que se deben de ejecutar en el programa.
+
+### VERSIÓN 0.0.1
+* Se mostrará el texto "Añadir una lista ...".
+* Al hacer click se debe ocultar el texto y mostrar un formulario.
+* El formulario está conformado por un input y un botón para que pueda añadir tareas a tu lista.
+
+### VERSIÓN 0.0.2
+* Al dar click en el botón de "Guardar", se mostrará un nuevo cuadro donde estará el nombre de la lista agregada.
+* Mostrar un texto de "Añadir una tarea" dentro de la lista.
+
+### VERSIÓN 0.0.3
+* Al dar click en "Añadir una tarea", deberá mostrar un formulario con un textarea y un botón que diga "Añadir".
+
+### VERSIÓN 0.0.4
+* Poner focus al input al dar click en "Agregar nueva tarea".
+* Al dar click en el botón de "Añadir", deberá aparecer el texto de la tarea debajo del título de la lista.
+
+### VERSIÓN 0.0.5
+* Mostrar el formulario nuevamente debajo de la última tarea añadida.
+
+### VERSIÓN 0.0.6 (EXTRA)
+* Poder agregar múltiples listas con tarjetas. Para esto, el formulario de "Añadir una lista" debe aparecer a la derecha de la lista anteriormente creada.
--- js/app.js
+++ js/app.js
@@ -1,16 +1,47 @@
-var addList = document.getElementsByClassName("addlist")[0];
+var addList = document.getElementById("addlist");
 var newList = document.getElementById("newlist");
+var saveButton = document.createElement("button");
+var cancelButton = document.createElement("button");
 
-newList.addEventListener('click', function(){
-  addList.className = "new";
-  var saveButton = document.createElement("button");
-  var cancelButton = document.createElement("button");
+/*Funcion para crear los botones para anadir una lista */
+
+function createButton(e){
+  var conteinerList = document.createElement("div");
+  conteinerList.className= "new";
   saveButton.type = "button";
   saveButton.className = "save";
   cancelButton.type = "button";
   cancelButton.className = "close"
-  addList.appendChild(saveButton);
-  addList.appendChild(cancelButton);
+  addList.appendChild(conteinerList)
+  conteinerList.appendChild(newList)
+  conteinerList.appendChild(saveButton);
+  conteinerList.appendChild(cancelButton);
   saveButton.appendChild(document.createTextNode ("Guardar"));
-  cancelButton.innerHTML = "&times;"
-});
+  cancelButton.innerHTML = "&times;" 
+  newList.removeEventListener("click",createButton);
+};
+
+newList.addEventListener('click',createButton);
+
+var newTaskBoard = document.getElementById("task-board");
+
+/* Funcion para agregar tarjetas a una lista*/
+
+function addTaskBoard(e){
+  var nameTaskBoard = document.getElementById("newlist").value;
+  document.getElementById("newlist").value = "";
+  var conteinerTask = document.createElement("div");
+  var tittleTaskBoard = document.createElement("h4");
+  var addTask = document.createElement("a");
+  addTask.href = "#";
+  conteinerTask.className = "inline";
+  tittleTaskBoard.appendChild(document.createTextNode(nameTaskBoard));
+  newTaskBoard.appendChild(conteinerTask);
+  conteinerTask.appendChild(tittleTaskBoard);
+  conteinerTask.appendChild(addTask);
+  addTask.appendChild(document.createTextNode("Añadir una tarjeta..."))
+}
+
+saveButton.addEventListener("click", addTaskBoard);
+
+
